f676f6fc8afda2b81c7a0472b0eeafd4ff7c72ad
Leonardo Tagliati <leotagliati56@gmail.com>
2024-05-16 15:29:53 -0300

    namePanelHandler finalizado

--- CharacterCreation.java
+++ CharacterCreation.java
@@ -1,13 +1,8 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -18,7 +13,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.xml.crypto.dsig.keyinfo.KeyName;
 
 public class CharacterCreation extends JFrame implements ActionListener{
     
@@ -65,23 +59,6 @@ public class CharacterCreation extends JFrame implements ActionListener{
         swordImage.setIcon(new ImageIcon("swordImage.png"));
         swordImage.setVisible(true);
 
-
-        nameTextfield = new JTextField();
-        nameTextfield.setBounds(25,10,250,40);
-        nameTextfield.setFont(new Font("Jet Brains Mono", Font.PLAIN, 30));
-        nameTextfield.setHorizontalAlignment(JTextField.CENTER);
-        nameTextfield.setEditable(false);
-        nameTextfield.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e){
-                nameTextfield.setEditable(true);
-            }});
-        nameTextfield.setVisible(true);
-
-        // Class Handler
-        
-
-        
         charNameButton = new JButton("Name");
         charClassButton = new JButton("Class");
         charGenderButton = new JButton("Gender");
@@ -103,23 +80,14 @@ public class CharacterCreation extends JFrame implements ActionListener{
 			jButton.setFont(new Font("Jet Brains Mono", Font.PLAIN, 15));
 			jButton.setFocusable(false);
         }
-        classHandler();
-        
         JPanel buttonsPanel = new JPanel();
 		buttonsPanel.setBounds(50, 150, 300, 500);
         buttonsPanel.setBackground(Color.BLACK);
 		buttonsPanel.setLayout(null);
-        
-        
-        // Painel gerado pela ActionListener do nameButton
-        namePanel = new JPanel();
-		namePanel.setBounds(400, 150, 300, 500);
-        namePanel.setBackground(Color.ORANGE);
-        namePanel.setLayout(null);
-        namePanel.add(nameTextfield);
-        namePanel.setVisible(false);
 
-        // Painel gerado pela ActionListener do classButton
+        namePanelHandler();
+        classPanelHandler();
+        
         
         for(int i = 0; i < charButtons.size(); i++)
         {
@@ -161,7 +129,7 @@ public class CharacterCreation extends JFrame implements ActionListener{
             }
         }
     }
-    public void classHandler()
+    public void classPanelHandler()
     {
         knightClassButton = new JButton("Knight");
         heraldClassButton = new JButton("Herald");
@@ -190,5 +158,29 @@ public class CharacterCreation extends JFrame implements ActionListener{
         {
             classesButtons.get(i).setBounds(50,100 + (i*60),200,50);
         }
+    }
+    public void namePanelHandler()
+    {
+
+        nameTextfield = new JTextField();
+        nameTextfield.setBounds(25,10,250,40);
+        nameTextfield.setFont(new Font("Jet Brains Mono", Font.PLAIN, 30));
+        nameTextfield.setHorizontalAlignment(JTextField.CENTER);
+        nameTextfield.setEditable(false);
+        nameTextfield.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e){
+                    nameTextfield.setEditable(true);
+            }});
+        nameTextfield.setVisible(true);
+
+
+        namePanel = new JPanel();
+		namePanel.setBounds(400, 150, 300, 500);
+        namePanel.setBackground(Color.ORANGE);
+        namePanel.setLayout(null);
+        namePanel.add(nameTextfield);
+        namePanel.setVisible(false);
+
     }
 }
