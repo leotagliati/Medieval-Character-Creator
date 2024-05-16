f167523b76f8cd01756afa71b01d776c062046fb
Leonardo Tagliati <leotagliati56@gmail.com>
2024-05-16 19:32:18 -0300

    Appearance finalizado

---
+++ Panels/AppearanceBodypart/Head.java
@@ -0,0 +1,11 @@
+package Panels.AppearanceBodypart;
+
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+
+public class Head extends BodyPart{
+
+    public Head() {
+        super("Head");
+    }
+}
--- Panels/AppearancePanel.java
+++ Panels/AppearancePanel.java
@@ -10,15 +10,21 @@ import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import Bodypart.Head;
+import Panels.AppearanceBodypart.BodyPart;
 
 public class AppearancePanel {
     private JPanel appearancePanel;
     
-    Head head = new Head();
+    private BodyPart eyes = new BodyPart("Eyes", 2);
+    private BodyPart skin = new BodyPart("Skin",3);
+    private BodyPart physic = new BodyPart("Physic", 1);
 
     public AppearancePanel()
     {
+        eyes.setup(0);
+        skin.setup(1);
+        physic.setup(2);
+
         this.appearancePanel = new JPanel();
 		this.appearancePanel.setBounds(400, 150, 300, 500);
         this.appearancePanel.setBackground(Color.GREEN);
@@ -26,9 +32,17 @@ public class AppearancePanel {
         this.appearancePanel.setVisible(false);
         
         
-        appearancePanel.add(this.head.getText());
-        appearancePanel.add(this.head.getSlider());
-        appearancePanel.add(this.head.getSliderPos());
+        appearancePanel.add(this.eyes.getBodyPartText());
+        appearancePanel.add(this.eyes.getBodyPartSlider());
+        appearancePanel.add(this.eyes.getBodyPartSliderValue());
+
+        appearancePanel.add(this.skin.getBodyPartText());
+        appearancePanel.add(this.skin.getBodyPartSlider());
+        appearancePanel.add(this.skin.getBodyPartSliderValue());
+
+        appearancePanel.add(this.physic.getBodyPartText());
+        appearancePanel.add(this.physic.getBodyPartSlider());
+        appearancePanel.add(this.physic.getBodyPartSliderValue());
     }
     public JPanel getPanel() {
         return appearancePanel;
