89d08494e109567f23bdcb54861ee1c304968983
Leonardo Tagliati <leotagliati56@gmail.com>
2024-05-20 11:16:54 -0300

    usando AudioHandler para trilha sonora

--- teste.java
+++ teste.java
@@ -9,14 +9,11 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 
+import Scripts.AudioHandler;
+
 public class teste {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        Scanner scanner = new Scanner(System.in);
         MainFrame c1 = new MainFrame();
-        File file  = new File("Music\\mainMenuOST.wav");
-        AudioInputStream backGroundMusic = AudioSystem.getAudioInputStream(file);
-        Clip clip = AudioSystem.getClip();
-        clip.open(backGroundMusic);
-        clip.start();
+        AudioHandler.tocamusica("Music\\mainMenuOST.wav");
     }
 }
