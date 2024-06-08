2970ee04df37dd3aa7dea5174cccdc897188a104
Leonardo Tagliati <leotagliati56@gmail.com>
2024-06-08 17:05:06 -0300

    connfact password

--- Scripts/Database/ConnFactory.java
+++ Scripts/Database/ConnFactory.java
@@ -8,7 +8,7 @@ import  java.sql.SQLException;
 public  class ConnFactory
 {   private static final String URL  =   "jdbc:mysql://localhost:3306/characterdb";
     private static final String USER =   "root";
-    private static final String PASS =   "root";
+    private static final String PASS =   "imtdb";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String ERROBD = "Erro na conexão com o Banco de Dados: "; 
 
