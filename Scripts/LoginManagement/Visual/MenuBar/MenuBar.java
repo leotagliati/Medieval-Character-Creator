fe8f01eb1cbb9b02be5be131445d84a3be0f18fa
Arthur Lee <138728172+Tutzjwlee@users.noreply.github.com>
2024-11-04 17:10:37 -0300

    sdasdsadasdsa

--- Scripts/LoginManagement/Visual/MenuBar/MenuBar.java
+++ Scripts/LoginManagement/Visual/MenuBar/MenuBar.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import Scripts.LoginManagement.Screens.TelaLogin;
+import Scripts.CharCreationManagement.Screens.MainMenu;
+import Scripts.CharCreationManagement.Screens.SearchCharacter;
 
 public class MenuBar extends JMenuBar {
 
     public MenuBar() {
         JMenu settingsMenu = new JMenu("Settings");
-
         JMenu languageMenu = new JMenu("Language");
 
         // Opções de idiomas
@@ -18,12 +19,18 @@ public class MenuBar extends JMenuBar {
         JMenuItem germanItem = new JMenuItem("Deutsch");
         JMenuItem frenchItem = new JMenuItem("Français");
         JMenuItem spanishItem = new JMenuItem("Español");
+        
 
-        // Action listeners para cada idioma
+        // Action listeners para cada idioma, passando o valor correto para MainMenu
         englishItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 TelaLogin.n = 1; // Define o idioma para Inglês
                 TelaLogin.getInstance().loadLanguage();
+                MainMenu.getInstance().setI(1);
+                MainMenu.getInstance().loadLanguage(1);
+                SearchCharacter.getInstance().setI(1);
+                SearchCharacter.getInstance().loadLanguage(1);
+                
             }
         });
 
@@ -31,6 +38,10 @@ public class MenuBar extends JMenuBar {
             public void actionPerformed(ActionEvent e) {
                 TelaLogin.n = 0; // Define o idioma para Português
                 TelaLogin.getInstance().loadLanguage();
+                MainMenu.getInstance().setI(0);
+                MainMenu.getInstance().loadLanguage(0);
+                SearchCharacter.getInstance().setI(0);
+                SearchCharacter.getInstance().loadLanguage(0);
             }
         });
 
@@ -38,6 +49,11 @@ public class MenuBar extends JMenuBar {
             public void actionPerformed(ActionEvent e) {
                 TelaLogin.n = 2; // Define o idioma para Alemão
                 TelaLogin.getInstance().loadLanguage();
+                MainMenu.getInstance().setI(2);
+                MainMenu.getInstance().loadLanguage(2);
+                SearchCharacter.getInstance().setI(2);
+                SearchCharacter.getInstance().loadLanguage(2);
+          
             }
         });
 
@@ -45,6 +61,10 @@ public class MenuBar extends JMenuBar {
             public void actionPerformed(ActionEvent e) {
                 TelaLogin.n = 3; // Define o idioma para Francês
                 TelaLogin.getInstance().loadLanguage();
+                MainMenu.getInstance().setI(3);
+                MainMenu.getInstance().loadLanguage(3);
+                SearchCharacter.getInstance().setI(3);
+                SearchCharacter.getInstance().loadLanguage(3);
             }
         });
 
@@ -52,6 +72,10 @@ public class MenuBar extends JMenuBar {
             public void actionPerformed(ActionEvent e) {
                 TelaLogin.n = 4; // Define o idioma para Espanhol
                 TelaLogin.getInstance().loadLanguage();
+                MainMenu.getInstance().setI(4);
+                MainMenu.getInstance().loadLanguage(4);
+                SearchCharacter.getInstance().setI(4);
+                SearchCharacter.getInstance().loadLanguage(4);
             }
         });
 
